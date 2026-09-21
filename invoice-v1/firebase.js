f11e54f330dea71c7d350dc0121176dadd88827e
MD ABDULLAH <kff138241@gmail.com>
2026-09-21 12:24:09 +0600

    Update firebase.js

--- invoice-v1/firebase.js
+++ invoice-v1/firebase.js
@@ -4,16 +4,16 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-aut
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
 // 🔴 এখানে আপনার নিজের Firebase config বসান
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT-default-rtdb.firebaseio.com",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "0000000000",
-  appId: "1:0000000000:web:xxxxxxxxxxxx"
+  apiKey: "AIzaSyCUW69PP3zxgUb3r2zWjXti6nYiWRm0L_4",
+  authDomain: "invoice-77f44.firebaseapp.com",
+  projectId: "invoice-77f44",
+  storageBucket: "invoice-77f44.firebasestorage.app",
+  messagingSenderId: "790061488061",
+  appId: "1:790061488061:web:da582bd2bbe04c4c92a4d4",
+  measurementId: "G-XKBVX8SN8B"
 };
-
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getDatabase(app);
